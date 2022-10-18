8f92376276cbaad5184920c973469910552bfc55
Rex-Breeze <horpeyeme599@gmail.com>
2022-10-18 18:28:08 +0100

    Regina Dennis

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-* main - prints _putchar
-* Return: always 0
+* main - prints _putchar and a new line
+* Return: Always 0
 */
-
 int main(void)
 {
     _putchar('_');
@@ -18,5 +17,4 @@ int main(void)
     _putchar('\n');
 
     return (0);
-    
 }
