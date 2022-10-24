37a3a9c4f25f2680dc6d2d6bf258cdc39809b0a2
Rex-Breeze <horpeyeme599@gmail.com>
2022-10-24 13:07:59 +0100

    Regina Dennis

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * _strlen - this function change the initial.
- * @s: this is a parameter to change.
- * Return: len
+ * _strlen - returns the length of a string
+ * @s: parameter
+ *
+ * Description: returns the length of the string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int len;
+	int l = 0;
 
-	while (*s != '\0')
-	{
-		len = len + 1;
-		s = s + 1;
-	}
-	return (len);
+	while (*s++)
+		l++;
+	return (l);
 }
