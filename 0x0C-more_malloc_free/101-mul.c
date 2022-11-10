b328369e684668bb659283d2cb7be6be9670d5aa
Rex-Breeze <horpeyeme599@gmail.com>
2022-11-10 15:04:56 +0100

    Regina Dennis

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,7 +13,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len);
  * @str: The string to be measured.
  *
  * Return: The length of the string.
- */
+*/
 int find_len(char *str)
 {
 	int len = 0;
@@ -32,7 +32,7 @@ int find_len(char *str)
  * Description: If there is insufficient space, the
  *              function exits with a status of 98.
  * Return: A pointer to the array.
- */
+*/
 char *create_xarray(int size)
 {
 	char *array;
@@ -57,7 +57,7 @@ char *create_xarray(int size)
  * @str: The string of numbers to be iterate through.
  *
  * Return: A pointer to the next non-zero element.
- */
+*/
 char *iterate_zeroes(char *str)
 {
 	while (*str && *str == '0')
@@ -73,7 +73,7 @@ char *iterate_zeroes(char *str)
  * Description: If c is a non-digit, the function
  *              exits with a status of 98.
  * Return: The converted int.
- */
+*/
 int get_digit(char c)
 {
 	int digit = c - '0';
@@ -96,7 +96,7 @@ int get_digit(char c)
  *
  * Description: If mult contains a non-digit, the function
  *              exits with a status value of 98.
- */
+*/
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
 	int mult_len, num, tens = 0;
@@ -141,7 +141,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
  * @final_prod: The buffer storing the running final product.
  * @next_prod: The next product to be added.
  * @next_len: The length of next_prod.
- */
+*/
 void add_nums(char *final_prod, char *next_prod, int next_len)
 {
 	int num, tens = 0;
@@ -186,7 +186,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  * Description: If the number of arguments is incorrect or one number
  *              contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
- */
+*/
 int main(int argc, char *argv[])
 {
 	char *final_prod, *next_prod;
