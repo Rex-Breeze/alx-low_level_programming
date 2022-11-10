1784ae86c7c420804810f8e5e936d2859f899e5d
Rex-Breeze <horpeyeme599@gmail.com>
2022-11-10 14:39:06 +0100

    Regina Dennis

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,41 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - Makes a new string containing `s1` and the first `n` bytes
- *                  of `s2`
- * @s1: Pointer to the first character of the first string
- * @s2: Pointer to the first character of the second string
- * @n: Number of bytes to copy from `s2`
- *
- * Return: Pointer to the first character in the concatonated string
+ * string_nconcat - fuction that concats two strings but with s2 by n amount.
+ * @s1: base sting to concat to. `Null` will return as empty.
+ * @s2: string to copy from. 'NULL' will return as empty string.
+ * @n: amount of s2 to copy.
+ * Return: pointer to allocated memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *cat;
-	unsigned int i, count = 0;
-	unsigned int len;
+	char *str;
+	unsigned int i, a, b;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	a = b = 0;
+	if (s1)
+		for (; s1[a]; a++)
+			;
+	if (s2)
+		for (; s2[b]; b++)
+			;
+	if (!(n >= b))
+		b = n;
 
-	len = _strlen(s2);
-	if (_strlen(s2) > n)
-		len = n;
-
-	cat = malloc(sizeof(char) * (_strlen(s1) + len + 1));
-
-	if (cat == NULL)
+	str = malloc(sizeof(char) * (a + b) + 1);
+	if (!str)
 		return (NULL);
 
-	for (; *s1; s1++, count++)
-		cat[count] = *s1;
+	for (i = 0; i < (a + b); i++)
+	{
+		if (i < a)
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - a];
+	}
 
-	for (i = 0; *s2 && i < n; i++, s2++, count++)
-		cat[count] = *s2;
+	str[i] = '\0';
 
-	cat[count] = '\0';
-
-	return (cat);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: pointer to first character of string
- *
- * Return: length of string
- */
-unsigned int _strlen(char *s)
-{
-	int i = 0;
-
-	for (; *s != '\0'; s++)
-		i++;
-
-	return (i);
+	return (str);
 }
