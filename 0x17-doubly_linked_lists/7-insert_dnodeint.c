30fd7bcbccef8baae88b5f17e6c154aa581147e8
Rex-Breeze <horpeyeme599@gmail.com>
2023-01-12 21:15:30 +0100

    Regina

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,6 +9,6 @@
  * Return:  the address of the new node, or NULL if it failed
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 }
